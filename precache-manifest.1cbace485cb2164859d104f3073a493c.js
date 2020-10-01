ddec2aebc13953d8a5946ca98dd96ad493b0df17
Daniel Rodrigues <daniel.rodrigues@memory.com.br>
2020-10-01 07:37:31 -0300

    Updates

---
+++ precache-manifest.1cbace485cb2164859d104f3073a493c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eedf512e52215def6723fd4969ee75b",
+    "url": "/movie-searcher-teste-dti/index.html"
+  },
+  {
+    "revision": "84e5016e320a65328a37",
+    "url": "/movie-searcher-teste-dti/static/css/main.ed619487.chunk.css"
+  },
+  {
+    "revision": "5d8768ca562cb2f66012",
+    "url": "/movie-searcher-teste-dti/static/js/2.2c58d989.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-searcher-teste-dti/static/js/2.2c58d989.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e5016e320a65328a37",
+    "url": "/movie-searcher-teste-dti/static/js/main.0aed8c46.chunk.js"
+  },
+  {
+    "revision": "381f0f4f0691c2139084",
+    "url": "/movie-searcher-teste-dti/static/js/runtime-main.b1c82ccf.js"
+  }
+]);
